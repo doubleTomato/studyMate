1a185dac09ec3ce76fc0e2a786d63eb6b6da8230
doubleTomato <doubletomato21@gmail.com>
2025-08-31 20:54:08 +0900

    [delete] breeze제거 후 백업

--- src/config/auth.php
+++ src/config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\Member::class),
+            'model' => env('AUTH_MODEL', App\Models\Members::class),
         ],
 
         // 'users' => [
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,18 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('home'); }); //home
+Route::get('/login', function () { return view('login'); }); //로그인
+Route::get('/signup', function () { return view('signup'); }); //회원가입
+Route::get('/write', function () { return view('write'); }); // 작성
+Route::get('/modify', function () { return view('modify'); }); // 수정
+Route::get('/detail', function () { return view('detail'); }); // 상세
+Route::get('/mypage', function () { return view('mypage'); }); // 마이페이지
+Route::get('/settings', function () { return view('settings'); }); // 설정
+Route::get('/search', function () { return view('search'); }); // 검색
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
