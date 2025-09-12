aa098a96ba7918a88ea9f9c44b4598245636da13
doubleTomato <doubletomato21@gmail.com>
2025-09-12 21:14:44 +0900

    [fix] 리스트 레이아웃 정리

--- src/resources/views/study/index.blade.php
+++ src/resources/views/study/index.blade.php
@@ -2,9 +2,11 @@
 @extends('layouts.app')
 @section('content')
 {{-- {{ dd($studies) }} --}}
-
+@php
+    $today = new DateTime();
+@endphp
 <section class="list-sec">
-    <h1>스터디 모집글</h1>
+    {{-- <h1>스터디 모집글</h1> --}}
     <div class="content-head flex-wrap">
         <ul class="filter-list">
             <li><input type="text" name="search"><i class="xi-search"></i></li>
@@ -23,22 +25,43 @@
     <div class="content">
         <ul class="list-wrap">
         @foreach($studies as $key => $val)
+            @php
+                $deadlineDate = DateTime::createFromFormat('Y-m-d',  $val['deadline']);
+                $d_day = $deadlineDate -> diff($today)
+            @endphp
             <li>
-                <div class="list-deadline">
-                    D- {{$val['deadline']}}
+                <div class="flex-wrap">
+                    <p class="list-tit">
+                        {{$val['title']}}
+                    </p>
+                    <div class="list-deadline">
+                        <span class="{{ $d_day -> invert === 0 ? 'cm-label deadline':'' }}">{{ $d_day -> invert === 0 ? '마감':$d_day -> format('D%R%a') }}</span> <span class="helper-text">({{ $val['deadline'] }})</span>
+                    </div>
                 </div>
-                <div class="list-tit">
-                    {{$val['title']}}
-                </div>
+                
                 <div>
-                    {{$val['owner_id']}}
+                    <p><i class="xi-marker-circle"></i></p>
+                    {{$val['region_id']}}
                 </div>
                 <div>
                 </div>
                 
                 <div>
-                    <p>스터디 기간</p>
+                    <p><i class="xi-calendar"></i></p>
                     {{ $val['start_date'] }} ~ {{ $val['end_date'] }}
+                </div>
+                <div>
+                    <p><i class="xi-community"></i></p>
+                    <div class="progress-bar">
+                        <div class="progress" style="width: {{-- (100 / $data['study']['max_members']) * count($data['participants']) --}}%"></div>
+                        <div>
+                            <span>{{-- count($val['participants']) --}}</span>
+                            <span>/</span>
+                            <span>
+                                {{ $val['max_members'] }}
+                            </span>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     {{$val['views']}}
