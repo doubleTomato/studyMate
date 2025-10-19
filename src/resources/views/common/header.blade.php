92d3b5d6c76e1c01f97fc4674bf41185ecf525dd
doubleTomato <doubleTomato21@gmail.com>
2025-10-19 21:36:37 +0900

    [fix] jquery ui 경로 http => https로 수정

--- src/resources/views/common/header.blade.php
+++ src/resources/views/common/header.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="{{ asset('xeicon/xeicon.min.css') }}">
     <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     {{-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script> --}}
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
