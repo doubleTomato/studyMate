f0bdf31794870e9686fddc6cd33d372cf5b8ca69
doubleTomato <doubleTomato21@gmail.com>
2025-10-20 23:05:27 +0900

    [fix] fly.toml에서 캐시 지우게 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,6 @@ set -x # 디버깅 용
 # 캐시 초기화 및 재생성
 cd /var/www/html/src
 
-echo " APP_ENV: $APP_ENV"
-echo "=========================================="
-
-php artisan config:clear
-php artisan cache:clear
-php artisan view:clear
-php artisan route:clear
-php artisan config:cache
 
 chown -R www-data:www-data /var/www/html/src/bootstrap/cache /var/www/html/src/storage
 #php artisan config:cache --env=production
