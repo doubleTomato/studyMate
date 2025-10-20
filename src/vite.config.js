922351fa46e41f772d426c82952999c8065d6ade
doubleTomato <doubleTomato21@gmail.com>
2025-10-20 13:56:26 +0900

    [fix] dockercompose 원복 및 vite에서 dev 설정 제거

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,24 +2,25 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',   // 외부 접근 허용
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      host: 'localhost', // 또는 127.0.0.1 (Windows 기준)
-    },
-    watch:{
-      usePolling: true,
-      interval: 500,
-        ignored: [
-        '**/vendor/**',
-        '**/node_modules/**',
-        '**/storage/**',
-        '**/public/**',
-    ],
-    }
-  },
+  // 개발환경에서만
+  // server: {
+  //   host: '0.0.0.0',   // 외부 접근 허용
+  //   port: 5173,
+  //   strictPort: true,
+  //   hmr: {
+  //     host: 'localhost', // 또는 127.0.0.1 (Windows 기준)
+  //   },
+  //   watch:{
+  //     usePolling: true,
+  //     interval: 500,
+  //       ignored: [
+  //       '**/vendor/**',
+  //       '**/node_modules/**',
+  //       '**/storage/**',
+  //       '**/public/**',
+  //   ],
+  //   }
+  // },
    build: {
     outDir: 'public/build',
     manifest: true,
