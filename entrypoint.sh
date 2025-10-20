986bc8add257c565ad12e9ce9944a5a669489bab
doubleTomato <doubleTomato21@gmail.com>
2025-10-20 20:15:06 +0900

    [fix] 스크립트 순서 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,10 @@
 set -e
 set -x # 디버깅 용
 
-
-echo "start php-fpm"
-php-fpm -D
-
-mkdir -p /var/log/nginx
-
-echo "sleep 5"
-sleep 5
-
-echo "start nginx..."
-# Nginx 실행
-nginx -g 'daemon off;'
 # 캐시 초기화 및 재생성
 cd /var/www/html/src
+
+rm -f .env
 
 echo " APP_ENV: $APP_ENV"
 echo "=========================================="
@@ -28,5 +18,18 @@ php artisan route:clear
 
 
 php artisan config:cache --env=production
+
+echo "start php-fpm"
+php-fpm -D
+
+mkdir -p /var/log/nginx
+
+echo "sleep 5"
+sleep 5
+
+echo "start nginx..."
+# Nginx 실행
+nginx -g 'daemon off;'
+
 
 exec php-fpm
