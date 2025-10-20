c83497021c4b60083aedda0d4777c230f04a0af9
doubleTomato <doubleTomato21@gmail.com>
2025-10-20 17:18:57 +0900

    [fix] 스크립트에서 env를 찍는 지 확인용 + 강제 할당

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,17 @@ echo "start nginx..."
 nginx -g 'daemon off;'
 # 캐시 초기화 및 재생성
 cd /var/www/html/src
+
+echo " APP_ENV: $APP_ENV"
+echo "=========================================="
+
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
-php artisan config:cache
+# php artisan config:cache
+
+
+php artisan config:cache --env=production
 
 exec php-fpm
