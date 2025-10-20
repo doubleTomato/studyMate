bf50ddb9c27cfa182956fd881f57fb1366c63909
doubleTomato <doubleTomato21@gmail.com>
2025-10-20 23:17:46 +0900

    [fix] view 경로 오류 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -x # 디버깅 용
 # 캐시 초기화 및 재생성
 cd /var/www/html/src
 
-
+mkdir -p /var/www/html/src/storage/framework/views
 chown -R www-data:www-data /var/www/html/src/bootstrap/cache /var/www/html/src/storage
 #php artisan config:cache --env=production
 
