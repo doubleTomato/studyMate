153c466fe638a72a42d960628dd9531b8f3edb82
doubleTomato <doubleTomato21@gmail.com>
2025-10-21 14:44:47 +0900

    [fix] vite경로 수정

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
         // fly.io서버에서만 실행
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
-            Vite::useBuildDirectory('build/.vite');
+            Vite::useManifestFilename('.vite/manifest.json');
         }
     }
 }
