0af73ac7dcf63c18f0e896d81d169d04b17b21cc
doubleTomato <doubleTomato21@gmail.com>
2025-10-21 14:26:39 +0900

    [fix] 스크립트 폴더 생성 주석처리 해제

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ set -x # 디버깅 용
 # 캐시 초기화 및 재생성
 cd /var/www/html/src
 
-#mkdir -p /var/www/html/src/storage/framework/views
-#mkdir -p /var/www/html/src/storage/framework/sessions
-#mkdir -p /var/www/html/src/storage/framework/cache/data
+mkdir -p /var/www/html/src/storage/framework/views
+mkdir -p /var/www/html/src/storage/framework/sessions
+mkdir -p /var/www/html/src/storage/framework/cache/data
 
 
-#chown -R www-data:www-data /var/www/html/src/bootstrap/cache /var/www/html/src/storage
+chown -R www-data:www-data /var/www/html/src/bootstrap/cache /var/www/html/src/storage
 #php artisan config:cache --env=production
 
 echo "start php-fpm"
